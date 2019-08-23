59dc289e4a817f4ff009eeebec6e060c222cffa6
Yanick Champoux <yanick@babyl.ca>
2019-08-23 12:26:57 -0400

    version tweaks

--- src/stories.js
+++ src/stories.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-//import Mermaid from './react-mermaid';
+import Mermaid from './react-mermaid';
 
 storiesOf('Mermaid', module).add('basic', () => {
-    return <div>Yo?</div>
-  // return <Mermaid name="stuff">
-  //  graph TD;
-  //         A-->B;
-  //         A-->C;
-  //         B-->D;
-  //         C-->D;
-  //       </Mermaid>;
+  return <Mermaid name="stuff">
+   graph TD;
+          A-->B;
+          A-->C;
+          B-->D;
+          C-->D;
+        </Mermaid>;
 })
